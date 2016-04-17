932d042ee36f0800b8581e1bdba57157e510d326
beomjun.gim3@gmail.com <beomjun.gim3@gmail.com>
2016-04-17 22:49:53 +0900

    .

--- src/onedayschool/settings/prod.py
+++ src/onedayschool/settings/prod.py
@@ -9,7 +9,7 @@ DATABASES = {
     'ENGINE': os.environ.get('DJANGO_DATABASE_ENGINE', 'django.db.backends.postgresql_psycopg2'),
     'NAME': os.environ.get('DJANGO_DATABASE_NAME', 'ubuntu'),
     'USER': os.environ.get('DJANGO_DATABASE_USER', 'ubuntu'),
-    'PASSWORD': os.environ.get('DJANGO_DATABASE_PASSWORD', 'withaskdjango!'),
+    'PASSWORD': os.environ.get('DJANGO_DATABASE_PASSWORD', 'asdf1515'),
     'HOST': os.environ.get('DJANGO_DATABASE_HOST', '127.0.0.1'),
     # 소스코드에 민감한 정보 넣어놓으면 ㄴㄴ 그런데 그냥 넣어놈 환경변수 공부해서 처리하기
     }
