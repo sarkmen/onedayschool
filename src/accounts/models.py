18ef6f4eeb4bd16d1ffcf2777e511caede71a065
beomjun.gim3@gmail.com <beomjun.gim3@gmail.com>
2016-05-31 20:00:15 +0900

    File import

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.core.files import File
 from django.dispatch import receiver
 from django.db.models.signals import post_save, pre_save
 from django.contrib.auth.models import User
